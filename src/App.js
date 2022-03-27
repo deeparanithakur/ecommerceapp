50e6231512ba0d60c0306c1585436073757f1a1f
deepa <deeparani10000@gmail.com>
2022-03-26 21:46:37 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './component/Navbar';
+import Home from './component/Home';
+import { Switch, Route } from 'react-router-dom';
+import Products from './component/Products';
+import Product from './component/Product';
+import Cart from './component/Cart';
+import Checkout from './component/Checkout';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Products} />
+        <Route exact path="/products/:id" component={Product} />
+        <Route exact path = "/cart" component={Cart} />
+        <Route exact path='/checkout' component={Checkout} />
+
+      </Switch>
+    </>
+
   );
 }
 
